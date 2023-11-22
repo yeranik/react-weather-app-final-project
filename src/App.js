8a4fc1904a8bb2e98647a0235ae4ce98f1ba121a
yeranik <136292592+yeranik@users.noreply.github.com>
2023-11-22 14:39:36 -0600

    added footer and github repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://mx.linkedin.com/in/yeranik" target="_blank">
+          Yerani K.{" "}
+        </a>{" "}
+        and is {""}
+        <a
+          href="https://github.com/yeranik/react-weather-app-final-project"
+          target="_blank"
+        >
+          open sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
