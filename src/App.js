cabb4d705fb2197990be96819193bee5c9657571
yeranik <136292592+yeranik@users.noreply.github.com>
2023-11-22 14:59:21 -0600

    added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://mx.linkedin.com/in/yeranik" target="_blank">
-          Yerani K.{" "}
-        </a>{" "}
-        and is {""}
-        <a
-          href="https://github.com/yeranik/react-weather-app-final-project"
-          target="_blank"
-        >
-          open sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://mx.linkedin.com/in/yeranik"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Yerani K.{" "}
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/yeranik/react-weather-app-final-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
