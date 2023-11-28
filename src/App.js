e7fcbeb5b1fadb32c90e530979a9f5e0784a62d1
yeranik <136292592+yeranik@users.noreply.github.com>
2023-11-27 22:30:59 -0600

    fixed component import

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Miami" />
+        <Weather defaultCity="Zapopan" />
 
         <footer>
           This project was coded by{" "}
